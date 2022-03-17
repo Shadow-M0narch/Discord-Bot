6507dbd38733e2610923f82bc67083a3088ffa4a
Shadow-M0narch <robin.schmid.business@gmail.com>
2022-03-17 13:38:36 +0100

    Changed login message

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("I have logged in as {0.user}".format(client))
+    print("Logged in as {0.user}".format(client))
 
 
 @client.event
